b3aef39237e7a822e32b45deb2653f22cf9ff577
John Doe <ben109092ben@gmail.com>
2025-02-17 10:56:45 +0800

    update

--- 250217/main.cpp
+++ 250217/main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main() {
-    int a = 100;
-    cout << "a = " << a;
+    // int a = 100;
+    // cout << "a = " << a << endl;
+    // a = 200;
+    // cout << "a = " << a;
+    int a = 100, b = 200;
+    cout << "a + b = " << a << " + " << b << " = " << a + b;
 }
